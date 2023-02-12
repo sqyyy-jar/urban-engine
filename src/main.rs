2368e4171d5a1c6db2f8e5f5970e19f0e1f03bbf
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-12 15:02:10 +0100

    Add testing main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use asm::{INSN_HALT, INSN_NOP};
+use context::{noverify::UnsafeContext, Context};
+
 pub mod asm;
 pub mod context;
 pub mod rt;
 pub mod stack;
 
 fn main() {
-    println!("Hello, world!");
+    let mut memory = vec![INSN_NOP, INSN_NOP, INSN_NOP, INSN_HALT];
+    let mut ctx = UnsafeContext::new(memory.as_mut_ptr(), memory.as_mut_ptr(), 1024);
+    loop {
+        ctx.decode_instruction()
+    }
 }
