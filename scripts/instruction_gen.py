2eeb8694ab6a84e0e493e76d09f7fc896b1021e6
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-12 11:34:53 +0100

    Add match arm generation

--- scripts/instruction_gen.py
+++ scripts/instruction_gen.py
@@ -110,9 +110,24 @@ def gen_trait_functions(value, id):
     return res
 
 
+def gen_match(scan_result):
+    res = "match insn {\n"
+    func_names = [insn["name"].replace(".", "_") for insn in scan_result]
+    const_names = [
+        insn["name"].upper().replace(".", "_").replace("IMM", "IMMEDIATE")
+        for insn in scan_result
+    ]
+    for i in range(len(const_names)):
+        res += f"    INSN_{const_names[i]}..=ENDINSN_{const_names[i]} => self.{func_names[i]}(insn),\n"
+    res += "    _ => {}\n}"
+    return res
+
+
 res = scan()
 # for i, x in enumerate(res):
 #     print(gen_insn(x, i))
 
-for i, x in enumerate(res):
-    print(gen_trait_functions(x, i))
+# for i, x in enumerate(res):
+#     print(gen_trait_functions(x, i))
+
+print(gen_match(res))
