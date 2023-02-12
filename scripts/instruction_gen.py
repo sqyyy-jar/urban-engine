bb22dcad79e72b5f4fe0d5df45d6112a0985f4cc
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-12 11:20:18 +0100

    Update generation script

--- scripts/instruction_gen.py
+++ scripts/instruction_gen.py
@@ -1,110 +1,118 @@
 import pprint
 
 instructions = {
-    'not': {'r': 2, 'i': 0, 'n': False},
-    'add': {'r': 3, 'i': 0, 'n': False},
-    'add_imm': {'r': 2, 'i': -1, 'n': True},
-    'sub': {'r': 3, 'i': 0, 'n': False},
-    'sub_imm': {'r': 2, 'i': -1, 'n': True},
-    'mul': {'r': 3, 'i': 0, 'n': False},
-    'mul_imm': {'r': 2, 'i': -1, 'n': True},
-    'div': {'r': 3, 'i': 0, 'n': False},
-    'div_imm': {'r': 2, 'i': -1, 'n': True},
-    'adds': {'r': 3, 'i': 0, 'n': False},
-    'adds_imm': {'r': 2, 'i': -1, 'n': True},
-    'subs': {'r': 3, 'i': 0, 'n': False},
-    'subs_imm': {'r': 2, 'i': -1, 'n': True},
-    'muls': {'r': 3, 'i': 0, 'n': False},
-    'muls_imm': {'r': 2, 'i': -1, 'n': True},
-    'divs': {'r': 3, 'i': 0, 'n': False},
-    'divs_imm': {'r': 2, 'i': -1, 'n': True},
-    'addf': {'r': 3, 'i': 0, 'n': False},
-    'subf': {'r': 3, 'i': 0, 'n': False},
-    'mulf': {'r': 3, 'i': 0, 'n': False},
-    'divf': {'r': 3, 'i': 0, 'n': False},
-    'and': {'r': 3, 'i': 0, 'n': False},
-    'or': {'r': 3, 'i': 0, 'n': False},
-    'xor': {'r': 3, 'i': 0, 'n': False},
-    'shl': {'r': 2, 'i': 6, 'n': True},
-    'shr': {'r': 2, 'i': 6, 'n': True},
-    'shrs': {'r': 2, 'i': 6, 'n': True},
-    'ldr': {'r': 2, 'i': 0, 'n': False},
-    'ldr_imm': {'r': 1, 'i': -1, 'n': True},
-    'mov': {'r': 2, 'i': 0, 'n': False},
-    'mov_imm': {'r': 1, 'i': -1, 'n': True},
-    'movs_imm': {'r': 1, 'i': -1, 'n': True},
-    'br': {'r': 1, 'i': 0, 'n': False},
-    'b_imm': {'r': 0, 'i': -1, 'n': True},
-    'brl': {'r': 1, 'i': 0, 'n': False},
-    'bl_imm': {'r': 0, 'i': -1, 'n': True},
-    'b.eq_imm': {'r': 1, 'i': -1, 'n': True},
-    'b.nq_imm': {'r': 1, 'i': -1, 'n': True},
-    'b.lt_imm': {'r': 1, 'i': -1, 'n': True},
-    'b.gt_imm': {'r': 1, 'i': -1, 'n': True},
-    'b.le_imm': {'r': 1, 'i': -1, 'n': True},
-    'b.ge_imm': {'r': 1, 'i': -1, 'n': True},
-    'nop': {'r': 0, 'i': 0, 'n': False},
-    'halt': {'r': 0, 'i': 0, 'n': False},
-    'syscall_imm': {'r': 0, 'i': 8, 'n': True},
+    "not": {"registers": 2, "number-arg": False},
+    "add": {"registers": 3, "number-arg": False},
+    "add_imm": {"registers": 2, "number-arg": True},
+    "sub": {"registers": 3, "number-arg": False},
+    "sub_imm": {"registers": 2, "number-arg": True},
+    "mul": {"registers": 3, "number-arg": False},
+    "mul_imm": {"registers": 2, "number-arg": True},
+    "div": {"registers": 3, "number-arg": False},
+    "div_imm": {"registers": 2, "number-arg": True},
+    "adds": {"registers": 3, "number-arg": False},
+    "adds_imm": {"registers": 2, "number-arg": True},
+    "subs": {"registers": 3, "number-arg": False},
+    "subs_imm": {"registers": 2, "number-arg": True},
+    "muls": {"registers": 3, "number-arg": False},
+    "muls_imm": {"registers": 2, "number-arg": True},
+    "divs": {"registers": 3, "number-arg": False},
+    "divs_imm": {"registers": 2, "number-arg": True},
+    "addf": {"registers": 3, "number-arg": False},
+    "subf": {"registers": 3, "number-arg": False},
+    "mulf": {"registers": 3, "number-arg": False},
+    "divf": {"registers": 3, "number-arg": False},
+    "and": {"registers": 3, "number-arg": False},
+    "or": {"registers": 3, "number-arg": False},
+    "xor": {"registers": 3, "number-arg": False},
+    "shl": {"registers": 2, "number-arg": True},
+    "shr": {"registers": 2, "number-arg": True},
+    "shrs": {"registers": 2, "number-arg": True},
+    "ldr": {"registers": 2, "number-arg": False},
+    "ldr_imm": {"registers": 1, "number-arg": True},
+    "str": {"registers": 2, "number-arg": False},
+    "str_imm": {"registers": 1, "number-arg": True},
+    "mov": {"registers": 2, "number-arg": False},
+    "mov_imm": {"registers": 1, "number-arg": True},
+    "movs_imm": {"registers": 1, "number-arg": True},
+    "br": {"registers": 1, "number-arg": False},
+    "b_imm": {"registers": 0, "number-arg": True},
+    "brl": {"registers": 1, "number-arg": False},
+    "bl_imm": {"registers": 0, "number-arg": True},
+    "b.eq_imm": {"registers": 1, "number-arg": True},
+    "b.nq_imm": {"registers": 1, "number-arg": True},
+    "b.lt_imm": {"registers": 1, "number-arg": True},
+    "b.gt_imm": {"registers": 1, "number-arg": True},
+    "b.le_imm": {"registers": 1, "number-arg": True},
+    "b.ge_imm": {"registers": 1, "number-arg": True},
+    "nop": {"registers": 0, "number-arg": False},
+    "halt": {"registers": 0, "number-arg": False},
+    "interrupt_imm": {"registers": 0, "number-arg": True},
 }
-
-
-def insn(key, value):
-    space = 32
-    registers = value['r']
-    value_size = value['i']
-    if value_size != -1:
-        space -= value_size
-    space -= registers * 5
-    space -= 6
-    return {
-        'name': key,
-        'space': space,
-        'value-size': value_size,
-        'registers': registers,
-        'number-arg': value['n'],
-    }
 
 
 def scan():
     all = []
     for key, value in instructions.items():
-        all.append(insn(key, value))
-    return sorted(all, key=lambda it: it['name'])
+        value["name"] = key
+        all.append(value)
+    return sorted(all, key=lambda it: it["name"])
 
 
 def gen_insn(value, id):
-    name = value['name']
-    registers = value['registers']
-    doc_name = name.replace('_imm', '')
-    const_name = name.upper().replace('.', '_').replace('IMM', 'IMMEDIATE')
-    res = f'/// {doc_name}'
+    name = value["name"]
+    registers = value["registers"]
+    doc_name = name.replace("_imm", "")
+    const_name = name.upper().replace(".", "_").replace("IMM", "IMMEDIATE")
+    res = f"/// {doc_name}"
     for i in range(registers):
         if i != 0:
-            res += ','
-        res += f' <X{i}>'
-    if value['number-arg']:
+            res += ","
+        res += f" <X{i}>"
+    if value["number-arg"]:
         if registers > 0:
-            res += ','
-        res += f' <i{32 - 6 - registers * 5}>'
-    res += '\npub const INSN_'
+            res += ","
+        res += f" <i{32 - 6 - registers * 5}>"
+    res += "\npub const INSN_"
     res += const_name
-    res += f': u32 = 0b{id:06b}_'
+    res += f": u32 = 0b{id:06b}_"
     for _ in range(32 - 6 - registers * 5):
-        res += '0'
+        res += "0"
     for _ in range(registers):
-        res += '_00000'
-    res += ';\npub const ENDINSN_'
+        res += "_00000"
+    res += ";\npub const ENDINSN_"
     res += const_name
-    res += f': u32 = 0b{id:06b}_'
+    res += f": u32 = 0b{id:06b}_"
     for _ in range(32 - 6 - registers * 5):
-        res += '1'
+        res += "1"
     for _ in range(registers):
-        res += '_11111'
-    res += ';'
+        res += "_11111"
+    res += ";"
+    return res
+
+
+def gen_trait_functions(value, id):
+    name = value["name"]
+    registers = value["registers"]
+    doc_name = name.replace("_imm", "")
+    res = f"    /// {doc_name}"
+    for i in range(registers):
+        if i != 0:
+            res += ","
+        res += f" <X{i}>"
+    if value["number-arg"]:
+        if registers > 0:
+            res += ","
+        res += f" <i{32 - 6 - registers * 5}>"
+    res += "\n    fn "
+    res += name.replace(".", "_")
+    res += "(&mut self, insn: u32);\n"
     return res
 
 
 res = scan()
+# for i, x in enumerate(res):
+#     print(gen_insn(x, i))
+
 for i, x in enumerate(res):
-    print(gen_insn(x, i))
+    print(gen_trait_functions(x, i))
