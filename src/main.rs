7c2017e593493caf653f1fc8d60511c663556a50
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-15 20:10:36 +0100

    Add modules

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::{env::args, fs};
 
 use context::{noverify::UnsafeContext, Context};
+use vmod::util::Util;
 
 pub mod asm;
 pub mod context;
 pub mod int;
 pub mod rt;
 pub mod stack;
+pub mod vmod;
 
 fn main() {
     let mut file = fs::read(args().nth(1).unwrap()).unwrap();
     let mut ctx = UnsafeContext::new(file.as_mut_ptr() as _, file.as_mut_ptr() as _, 8192);
+    ctx.load_vmod(&Util);
     loop {
-        // dbg!(ctx.mem);
         ctx.decode_instruction()
     }
 }
