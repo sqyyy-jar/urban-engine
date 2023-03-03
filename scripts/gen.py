d75a45940e11d9138e9e48d618dbb4c4ffd3bca3
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-03-03 21:35:39 +0100

    Remove old python scripts

--- scripts/gen.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import instruction_gen
-import interrupt_gen
-
-with open("src/asm.rs", mode="w") as f:
-    instruction_gen.write(f)
-with open("src/int.rs", mode="w") as f:
-    interrupt_gen.write(f)
